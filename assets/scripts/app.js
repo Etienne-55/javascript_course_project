8b6c2442393a36094d630f7439980d3abe0a9ecd
etienne <etienne.pautet@gmail.com>
2025-07-10 13:39:44 -0300

    Ex01

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,4 +5,15 @@ currentResult = currentResult - 2;
 
 let printDefaultResult = `${defaultResult}`;
 
-outputResult(currentResult, defaultResult);
+outputResult(currentResult, defaultResult);  
+
+let userInput = 15;
+
+let result;
+
+result = 18 + userInput;
+
+result = result - 16;
+
+alert(result);
+alert(userInput)
