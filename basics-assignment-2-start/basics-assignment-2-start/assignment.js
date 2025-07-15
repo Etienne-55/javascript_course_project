dd42757f5123875550b54b7d5f389382d608324f
etienne <etienne.pautet@gmail.com>
2025-07-15 13:49:21 -0300

    Js/assignment

---
+++ basics-assignment-2-start/basics-assignment-2-start/assignment.js
@@ -0,0 +1,17 @@
+const task3Element = document.getElementById('task-3');
+
+function hello_there(name){
+
+    alert("Hello")
+
+}
+
+function hello_name(name){
+
+    alert(name)
+
+}
+
+
+hello_there()
+hello_name("lex")
