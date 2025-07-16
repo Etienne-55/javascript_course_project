4e8c6c725a49d659cacb3ea3c0a246ea0e4347f4
etienne <etienne.pautet@gmail.com>
2025-07-16 17:23:15 -0300

    js functions and css sections

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,21 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add() {
-    currentResult = currentResult + parseInt(userInput.value);
-    outputResult(currentResult, '')
+
+function getUserNumberInput() {
+    return parseInt(userInput.value);
 }
 
-addBtn.addEventListener('click',  add );
+function add() {
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = `${currentResult} + ${enteredNumber}` 
+    currentResult = currentResult + enteredNumber;
+    outputResult(currentResult, calcDescription)
+}
+
+function subtract() {
+
+}
+
+
+addBtn.addEventListener('click',  add);
